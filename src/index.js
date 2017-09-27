3e5997dd3e9958084e4fcf0faa790699ee72f755
Edson Júnior <ejunior.batista@gmail.com>
2017-09-27 10:58:50 -0300

    Ajustes nas dependências do npm

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/app";
-import reducers from "./reducers/root";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>,
-  document.querySelector(".container-fluid")
-);
+ReactDOM.render(<App />, document.querySelector(".container-fluid"));
 
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
